b9c5b2169092c43e26cabc2ba6412a6e1b6cb5c1
Philip <hoangphat1908@gmail.com>
2017-04-16 01:08:16 -0700

    new game

---
+++ core/src/com/mygdx/mygame/MyGame.java
@@ -0,0 +1,31 @@
+package com.mygdx.mygame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.mygame.Screens.PlayScreen;
+
+public class MyGame extends Game {
+	public SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ core/src/com/mygdx/mygame/Screens/PlayScreen.java
@@ -0,0 +1,62 @@
+package com.mygdx.mygame.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.mygame.MyGame;
+
+/**
+ * Created by hoangphat1908 on 4/16/2017.
+ */
+
+public class PlayScreen implements Screen {
+    private MyGame game;
+    Texture texture;
+
+    public PlayScreen(MyGame game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
